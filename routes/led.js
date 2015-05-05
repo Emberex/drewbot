e3bd9bb0c0f8adcfa4c2306df136b51e289f03d2
Erik May <erik.may@emberex.com>
2015-05-04 23:36:22 -0700

    Toggling an LED from the browser

--- routes/led.js
+++ routes/led.js
@@ -1,13 +1,34 @@
 var express = require('express');
 var drewbotUtils = require('./../backend/src/drewbotUtils');
+
+var serialPort = require('serialport');
+var port;
+serialPort.list(function(err, ports) {
+    console.log("Opening serial port: ", ports.slice(-1)[0].comName);
+    port = new serialPort.SerialPort(ports.slice(-1)[0].comName, null, false);
+    port.open(function(err) { 
+        console.log("on open err: ", err);
+    });
+});
+
+
 var router = express.Router();
 
 router.post('/', function(req, res) {
+    
+    port.on('data', function(data) {
+        console.log('data received: ' + data);
+    });
     
     var toggle = "off";
     if(req.body.toggleOn) {
         toggle = "on";
     }
+    
+    port.write(toggle+"\n", function(err, results) {
+        console.log('err ' + err);
+        console.log('results ' + results);
+    });
     res.send('Turning LED ' + toggle + ' ...'+ drewbotUtils.getCurrentFormattedTime());
 });
 
