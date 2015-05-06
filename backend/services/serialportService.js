b5e12d617fc8e1e1798364199a6b8be46182214e
Erik May <erik.may@emberex.com>
2015-05-06 08:57:38 -0700

    fixed issue: web server would not start if there were no devices on the serial ports

--- backend/services/serialportService.js
+++ backend/services/serialportService.js
@@ -3,15 +3,19 @@ var serialPort = require('serialport');
 var port;
 
 serialPort.list(function(err, ports) {
-    console.log("Opening serial port: ", ports.slice(-1)[0].comName);
-    port = new serialPort.SerialPort(ports.slice(-1)[0].comName, null, false);
-    port.open(function(err) { 
-        if(err) {
-            console.log("on open err: ", err);
-        } else {
-            console.log("serial port opened successfully!");
-        }
-    });
+    if(ports) {
+        console.log("Opening serial port: ", ports.slice(-1)[0].comName);
+        port = new serialPort.SerialPort(ports.slice(-1)[0].comName, null, false);
+        port.open(function(err) { 
+            if(err) {
+                console.log("on open err: ", err);
+            } else {
+                console.log("serial port opened successfully!");
+            }
+        });
+    } else {
+        console.log("No devices found on serial ports.");
+    }
 });
 
 
